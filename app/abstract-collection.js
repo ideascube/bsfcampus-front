d09c6c68c7a4e42b35286e5d480c75718c9634c8
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-03-15 23:35:24 +0100

    Allow collections to have meta information, and bug fix in collection parser

--- app/abstract-collection.js
+++ app/abstract-collection.js
@@ -11,7 +11,25 @@ define(
 			// #FIXME: This parameter should be externalized in a static config file
 			serverGateway: 'http://localhost:5000',
 
+			initialize: function() {
+				this._meta = {};
+			},
+
+			 meta: function(prop, value) {
+				if (value === undefined) {
+					return this._meta[prop];
+				} else {
+					this._meta[prop] = value;
+				}
+			},
+
 			parse: function(response, options) {
+				if (options) {
+					if (options.jsonKey) {
+						return response[options.jsonKey];
+					}
+				}
+				
 				return response[this.jsonKey];
 			},
 
