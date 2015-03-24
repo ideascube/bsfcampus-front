2659a668287d5e80fb0f6557f66a0ebed44fa161
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-03-24 13:15:21 +0100

    Gives the result when we answer a question (but not the complete feedback yet) and allows to move to the next question. The case of the last question is not managed yet. A little bit messy, needs to be cleaned up.

--- pods/exercise-attempt/view.js
+++ pods/exercise-attempt/view.js
@@ -30,51 +30,89 @@ define(
 					resource: this.resource.forTemplate(),
 				});
 				this.$el.html(html);
+				this.findCurrentQuestion();
 				this.renderCurrentQuestion();
+
+				return this;
 			},
 
 			events: {
 				'submit form': 'submitAnswer',
+				'click .btn-continue': 'continueExercise',
 			},
 
-			renderCurrentQuestion: function() {
+			findCurrentQuestion: function() {
 				var question_answers = this.model.get('question_answers');
 				for (var i = 0; i < question_answers.length; i++) {
-					if (question_answers[i].question != null) {
-						var questionModel = new ExerciseAttemptQuestionModel(question_answers[i].question);
-						this.renderQuestion(questionModel);
-						return;
+					if (question_answers[i].given_answer == null) {
+						this.currentQuestionModel = new ExerciseAttemptQuestionModel(question_answers[i].question);
+						return this.currentQuestionModel;
 					}
 				}
 			},
 
-			renderQuestion: function(questionModel) {
-				var questionView = new ExerciseAttemptQuestionView({model: questionModel});
+			renderCurrentQuestion: function() {
+				var questionView = new ExerciseAttemptQuestionView({model: this.currentQuestionModel});
 				questionView.render();
-				this.$el.find('.exercise-attempt-footer').addClass('hidden');
-				this.$el.find('.exercise-attempt-question-answer-feedback').addClass('hidden');
-				this.$el.find('.exercise-attempt-form').removeClass('hidden');
+
+				this.$el.find('#current_question_id_input').val(this.currentQuestionModel.id);
 				this.$el.find('.exercise-attempt-question').html(questionView.$el);
-				this.$el.find('#current_question_id_input').val(questionModel.id);
+
+				this.$el.find('.exercise-attempt-question-answer-feedback').addClass('hidden');
+				this.$el.find('.exercise-attempt-footer').addClass('hidden');
+				this.$el.find('.exercise-attempt-form').removeClass('hidden');
+			
+				return this;
+			},
+
+			renderFeedback: function(questionId) {
+				console.log("looking for question", questionId);
+				var question_answers = this.model.get('question_answers');
+				for (var i = 0; i < question_answers.length; i++) {
+					if (question_answers[i].question_id == questionId) {
+						console.log("Found it!");
+						var answered_correctly = question_answers[i].is_answered_correctly;
+						console.log("Answer OK?", answered_correctly);
+					}
+				}
+				var resultText = '?';
+				if (answered_correctly === true) { resultText = 'Good answer!'; }
+				else if (answered_correctly === false) { resultText = 'Wrong answer!'; }
+				this.$el.find('.exercise-attempt-question-answer-feedback').html("COUCOU"); // FIXME: Give actual feedback
+				this.$el.find('.exercise-attempt-question-answer-result').html(resultText);
+				this.$el.find('.exercise-attempt-question-answer-feedback').removeClass('hidden');
+				this.$el.find('.exercise-attempt-footer').removeClass('hidden');
+				this.$el.find('.exercise-attempt-form').addClass('hidden');
 			},
 
 			submitAnswer: function(e) {
-				e.preventDefault();
-				console.log("Tried to submit answer");
+				e.preventDefault(); // Prevents from submitting the form
+				
+				var currentQuestionId = this.currentQuestionModel.id;
 				var formData = this.$el.find('form').serialize();
-				console.log(formData);
+				var self = this;
 				$.ajax({
 						type: 'POST',
 						url: this.model.postAnswerUrl(), 
 						data: formData,
 						dataType: 'json'
 					}).done(function(result){
-						console.log(result);
+						var parsedModel = new ExerciseAttemptModel().parse(result);
+						self.model = new ExerciseAttemptModel(parsedModel);
+						self.renderFeedback(currentQuestionId);
 					}).fail(function(error){
 						console.log(error);
 					});
-				return false;
-			}
+
+				return false; // Also prevents from submitting the form
+				              // Not sure if useful
+			},
+
+			continueExercise: function() {
+				// By default, this.render() renders the first unanswered question.
+				// So we just need to refresh the view.
+				this.render();
+			},
 
 		});
 		
