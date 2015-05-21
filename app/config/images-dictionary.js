dbd444b5988879448c63e15fe5ec68739122b029
Frédéric Fourcade <frederic.fourcade@gmail.com>
2015-05-21 17:13:55 +0200

    adds right bg

--- app/config/images-dictionary.js
+++ app/config/images-dictionary.js
@@ -7,7 +7,7 @@ define(
 		return {
 
 			placeholder48: 'http://placehold.it/48x48/1caff6/ffffff',
-			default_background_image: Constants.imagesPath + 'bg_city.png',
+			default_background_image: Constants.imagesPath + 'city.png',
 			logo_gates_foundation: Constants.imagesPath + 'gatesfound.png',
 			logo_bsf_campus: Constants.imagesPath + 'BSF_logo_header.png',
 			logo_bsf: Constants.imagesPath + 'BSF_logo_footer.png',
