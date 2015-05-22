daf717983e56babb724ad0d7bfd97e49cba75a6c
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-05-22 18:43:00 +0200

    Adds config file.

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,9 +1,18 @@
-define(['lib/window'],
-	function (w) {
+define(['lib/window', 'config'],
+	function (w, config) {
+
+		var port = 5000;
+		if ('server_port' in config) {
+			port = config.server_port;
+		} else if ('server_type' in config) {
+			if (config.server_type == 'local') {
+				port = 5001;
+			}
+		}
 
 		return {
 
-			serverGateway: w.location.protocol + '//' + w.location.hostname  + ':5001',
+			serverGateway: w.location.protocol + '//' + w.location.hostname  + ':' + port,
 
 			imagesPath: 'assets/images/'
 
---
+++ config_dist.js
@@ -0,0 +1,17 @@
+// Make a copy of this file called config.js and customize configuration.
+
+define(function(){ 
+
+	var config = {};
+
+	config.server_type = 'central';
+
+	/*
+	Uncomment the line below if the server is on a custom port.
+	Default is 5001 is server_type == 'local', 5000 otherwise.
+	*/
+	// var server_port = 5000
+
+	return config;
+
+})
