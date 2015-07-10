c241e4191a1c65a5d0be7452ae92e76e04778f80
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-07-10 15:02:32 +0200

    Updates distribution config file to allow for setting an API path

--- config_dist.js
+++ config_dist.js
@@ -10,8 +10,10 @@ define(function(){
 	Uncomment the line below if the server is on a custom port.
 	Default is 5001 is server_type == 'local', 5000 otherwise.
 	*/
-	// var server_port = 5000
+	// config.server_port = 5000;
+
+	config.api_path = '';
 
 	return config;
 
-})
+});
