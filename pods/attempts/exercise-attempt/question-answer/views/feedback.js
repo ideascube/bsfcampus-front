b979e1f0a36f72b7fd246efd152ae97a5c3cdc5b
fredfourcade <frederic.fourcade@gmail.com>
2015-08-13 16:58:22 +0200

    [#MOOKBSF-536] When using ViewManager for the questions views, some JQuery functions (Draggable, Dropdowns) didn't work anymore, so I reversed these classes

--- pods/attempts/exercise-attempt/question-answer/views/feedback.js
+++ pods/attempts/exercise-attempt/question-answer/views/feedback.js
@@ -3,7 +3,6 @@ define(
 		'jquery',
 		'underscore',
 		'backbone',
-        'viewmanager',
 		'app/config',
 
 		'pods/attempts/exercise-attempt/question-answer/unique-answer-mcq/views/feedback',
@@ -13,48 +12,27 @@ define(
 		'pods/attempts/exercise-attempt/question-answer/ordering/views/feedback',
 		'pods/attempts/exercise-attempt/question-answer/categorizer/views/feedback'
 	],
-	function($, _, Backbone, VM, Config,
+	function($, _, Backbone, Config,
 		UniqueAnswerMCQView, MultipleAnswerMCQView, RightOrWrongView, DropdownView, OrderingView, CategorizerView
 		) {
 
 		return {
 
 			initialize: function(model) {
-                var rv = null;
 				switch (model.questionModel().get('_cls')) {
-                    case 'UniqueAnswerMCQExerciseQuestion':
-                        rv = VM.createView(Config.constants.VIEWS_ID.QUESTIONS.UNIQUE_ANSWER_MCQ, function() {
-                            return new UniqueAnswerMCQView({model: model});
-                        });
-                        break;
+					case 'UniqueAnswerMCQExerciseQuestion':
+						return new UniqueAnswerMCQView({model: model});
 					case 'MultipleAnswerMCQExerciseQuestion':
-                        rv = VM.createView(Config.constants.VIEWS_ID.QUESTIONS.MULTIPLE_ANSWER_MCQ, function() {
-                            return new MultipleAnswerMCQView({model: model});
-                        });
-                        break;
+						return new MultipleAnswerMCQView({model: model});
 					case 'RightOrWrongExerciseQuestion':
-                        rv = VM.createView(Config.constants.VIEWS_ID.QUESTIONS.RIGHT_OR_WRONG, function() {
-                            return new RightOrWrongView({model: model});
-                        });
-                        break;
+						return new RightOrWrongView({model: model});
 					case 'DropdownExerciseQuestion':
-                        rv = VM.createView(Config.constants.VIEWS_ID.QUESTIONS.DROPDOWN, function() {
-                            return new DropdownView({model: model});
-                        });
-                        break;
+						return new DropdownView({model: model});
 					case 'OrderingExerciseQuestion':
-                        rv = VM.createView(Config.constants.VIEWS_ID.QUESTIONS.ORDERING, function() {
-                            return new OrderingView({model: model});
-                        });
-                        break;
+						return new OrderingView({model: model});
 					case 'CategorizeExerciseQuestion':
-                        rv = VM.createView(Config.constants.VIEWS_ID.QUESTIONS.CATEGORIZER, function() {
-                            return new CategorizerView({model: model});
-                        });
-                        break;
+						return new CategorizerView({model: model});
 				}
-
-                return rv;
 			},
 
 		};
--- pods/attempts/exercise-attempt/question-answer/views/form.js
+++ pods/attempts/exercise-attempt/question-answer/views/form.js
@@ -3,8 +3,7 @@ define(
 		'jquery',
 		'underscore',
 		'backbone',
-        'viewmanager',
-        'app/config',
+		'app/config',
 
 		'pods/attempts/exercise-attempt/question-answer/unique-answer-mcq/views/form',
 		'pods/attempts/exercise-attempt/question-answer/multiple-answer-mcq/views/form',
@@ -13,47 +12,35 @@ define(
 		'pods/attempts/exercise-attempt/question-answer/ordering/views/form',
 		'pods/attempts/exercise-attempt/question-answer/categorizer/views/form',
 	],
-	function($, _, Backbone, VM, Config,
+	function($, _, Backbone, Config,
 		UniqueAnswerMCQView, MultipleAnswerMCQView, RightOrWrongView, DropdownView, OrderingView, CategorizerView
 		) {
 
 		return {
 
 			initialize: function(model) {
-				var rv = null;
+				var ret = null;
 				switch (model.questionModel().get('_cls')) {
 					case 'UniqueAnswerMCQExerciseQuestion':
-                        rv = VM.reuseView(Config.constants.VIEWS_ID.QUESTIONS.UNIQUE_ANSWER_MCQ, function() {
-                            return new UniqueAnswerMCQView({model: model});
-                        });
+						ret = new UniqueAnswerMCQView({model: model});
 						break;
 					case 'MultipleAnswerMCQExerciseQuestion':
-                        rv = VM.reuseView(Config.constants.VIEWS_ID.QUESTIONS.MULTIPLE_ANSWER_MCQ, function() {
-                            return new MultipleAnswerMCQView({model: model});
-                        });
+						ret = new MultipleAnswerMCQView({model: model});
 						break;
 					case 'RightOrWrongExerciseQuestion':
-                        rv = VM.reuseView(Config.constants.VIEWS_ID.QUESTIONS.RIGHT_OR_WRONG, function() {
-                            return new RightOrWrongView({model: model});
-                        });
+						ret = new RightOrWrongView({model: model});
 						break;
 					case 'DropdownExerciseQuestion':
-                        rv = VM.reuseView(Config.constants.VIEWS_ID.QUESTIONS.DROPDOWN, function() {
-                            return new DropdownView({model: model});
-                        });
+						ret = new DropdownView({model: model});
 						break;
 					case 'OrderingExerciseQuestion':
-                        rv = VM.reuseView(Config.constants.VIEWS_ID.QUESTIONS.ORDERING, function() {
-                            return new OrderingView({model: model});
-                        });
+						ret = new OrderingView({model: model});
 						break;
 					case 'CategorizeExerciseQuestion':
-                        rv = VM.reuseView(Config.constants.VIEWS_ID.QUESTIONS.CATEGORIZER, function() {
-                            return new CategorizerView({model: model});
-                        });
+						ret = new CategorizerView({model: model});
 						break;
 				}
-				return rv;
+				return ret;
 			}
 
 		};
