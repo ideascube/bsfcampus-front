ab38828caaa5f3301c7a0dd0086d63679e4cdf46
Arnaud Riegert <arnaud.riegert@gmail.com>
2015-10-28 00:37:46 +0100

    Removes some old asset images

--- app/config/images-dictionary.js
+++ app/config/images-dictionary.js
@@ -6,20 +6,14 @@ define(
 
 		return {
 
-			placeholder48: 'http://placehold.it/48x48/1caff6/ffffff',
 			default_background_image: Constants.imagesPath + 'city.png',
 			logo_gates_foundation: Constants.imagesPath + 'gatesfound.png',
 			logo_bsf_campus: Constants.imagesPath + 'BSF_logo_header.png',
 			logo_bsf: Constants.imagesPath + 'Logo-BSF_small.png',
-			bell: Constants.imagesPath + 'bell.png',
-			bellRed: Constants.imagesPath + 'bellRed.png',
 
 			arrow: Constants.imagesPath + 'arrow.png',
-			arrowInCircle: Constants.imagesPath + 'iconmonstr-arrow-28-icon-256.png',
 			studentHat: Constants.imagesPath + 'student-hat.png',
 			quill: Constants.imagesPath + 'iconmonstr-pen-12-icon-256.png',
-			checkStroke: Constants.imagesPath + 'iconmonstr-check-mark-4-icon-256.png',
-			checkFull: Constants.imagesPath + 'validate.png',
 			checkSingle: Constants.imagesPath + 'iconmonstr-check-mark-6-icon-256.png',
 			greenCheck: Constants.imagesPath + 'check_green.png',
 			wrong: Constants.imagesPath + 'wrong-icon.png',
@@ -27,7 +21,6 @@ define(
 			certificate: Constants.imagesPath + 'iconmonstr-certificate.png',
 			lock: Constants.imagesPath + 'iconmonstr-lock-3-icon-256.png',
 			download: Constants.imagesPath + 'download_arrow.png',
-			orderingExerciseBg: Constants.imagesPath + 'carre_ligne.png',
             facebookLogo: Constants.imagesPath + 'icon_facebook.png',
             twitterLogo: Constants.imagesPath + 'icon_twitter.png',
             home: {
